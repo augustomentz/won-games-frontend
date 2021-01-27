7c6a55948913e7425ab8517fb75b00aec9a7e9fe
Augusto Mentz <augustohmn.tech@gmail.com>
2021-01-27 13:14:30 -0300

    TextContent component

---
+++ src/components/TextContent/stories.tsx
@@ -0,0 +1,19 @@
+import { Meta, Story } from '@storybook/react/types-6-0'
+import TextContent, { TextContentProps } from '.'
+
+import TextContentMock from './mock'
+
+export default {
+	title: 'TextContent',
+	component: TextContent,
+	args: TextContentMock,
+	parameters: {
+		backgrounds: {
+			default: 'won-dark'
+		}
+	}
+} as Meta
+
+export const Default: Story<TextContentProps> = (args) => (
+	<TextContent {...args} />
+)
