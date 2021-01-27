680b0a6ec5b6dc00d6a16fd30b1d859c2d00a49f
Augusto Mentz <augustohmn.tech@gmail.com>
2021-01-27 11:15:56 -0300

    GameDetails component

---
+++ src/components/GameDetails/stories.tsx
@@ -0,0 +1,38 @@
+import { Meta, Story } from '@storybook/react/types-6-0'
+import GameDetails, { GameDetailsProps } from '.'
+
+import GameDetailsMock from './mock'
+
+export default {
+	title: 'Game/GameDetails',
+	component: GameDetails,
+	parameters: {
+		backgrounds: {
+			default: 'won-dark'
+		}
+	},
+	args: GameDetailsMock,
+	argTypes: {
+		releaseDate: {
+			control: 'date'
+		},
+		platforms: {
+			control: {
+				type: 'inline-check',
+				options: ['windows', 'apple', 'linux']
+			}
+		},
+		genres: {
+			control: {
+				type: 'inline-check',
+				options: ['Role-playing', 'Narrative']
+			}
+		}
+	}
+} as Meta
+
+export const Default: Story<GameDetailsProps> = (args) => (
+	<div style={{ maxWidth: '130rem', margin: '0 auto' }}>
+		<GameDetails {...args} />
+	</div>
+)
---
+++ src/components/GameDetails/test.tsx
@@ -0,0 +1,77 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import GameDetails, { GameDetailsProps } from '.'
+
+const props: GameDetailsProps = {
+	developer: 'Different Tales',
+	releaseDate: '2020-11-21T23:00:00',
+	rating: 'BR0',
+	publisher: '2K',
+	platforms: ['windows', 'apple', 'linux'],
+	genres: ['Role-playing', 'Narrative']
+}
+
+describe('<GameDetails />', () => {
+	it('Should render the blocks', () => {
+		renderWithTheme(<GameDetails {...props} />)
+
+		expect(
+			screen.getByRole('heading', { name: /developer/i })
+		).toBeInTheDocument()
+
+		expect(
+			screen.getByRole('heading', { name: /release date/i })
+		).toBeInTheDocument()
+
+		expect(
+			screen.getByRole('heading', { name: /platforms/i })
+		).toBeInTheDocument()
+
+		expect(
+			screen.getByRole('heading', { name: /publisher/i })
+		).toBeInTheDocument()
+
+		expect(screen.getByRole('heading', { name: /rating/i })).toBeInTheDocument()
+
+		expect(screen.getByRole('heading', { name: /genre/i })).toBeInTheDocument()
+	})
+
+	it('Should render the platform icons', () => {
+		renderWithTheme(<GameDetails {...props} />)
+
+		expect(screen.getByRole('img', { name: /windows/i })).toBeInTheDocument()
+		expect(screen.getByRole('img', { name: /apple/i })).toBeInTheDocument()
+		expect(screen.getByRole('img', { name: /linux/i })).toBeInTheDocument()
+	})
+
+	it('Should render the formated date', () => {
+		renderWithTheme(<GameDetails {...props} />)
+
+		expect(screen.getByText('Nov 21, 2020')).toBeInTheDocument()
+	})
+
+	it('Should render free rating when BR0', () => {
+		renderWithTheme(<GameDetails {...props} />)
+
+		expect(screen.getByText(/free/i)).toBeInTheDocument()
+	})
+
+	it('Should render 16+ rating when BR16', () => {
+		renderWithTheme(<GameDetails {...props} rating="BR16" />)
+
+		expect(screen.getByText(/16\+/i)).toBeInTheDocument()
+	})
+
+	it('Should render 18+ rating when BR18', () => {
+		renderWithTheme(<GameDetails {...props} rating="BR18" />)
+
+		expect(screen.getByText(/18\+/i)).toBeInTheDocument()
+	})
+
+	it('Should render a list of genres', () => {
+		renderWithTheme(<GameDetails {...props} />)
+
+		expect(screen.getByText('Role-playing / Narrative')).toBeInTheDocument()
+	})
+})
