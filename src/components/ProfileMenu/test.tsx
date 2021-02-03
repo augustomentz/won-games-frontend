9f327f38839df775f4e27d43ddc0fbb842ded5a3
Augusto Mentz <augustohmn.tech@gmail.com>
2021-02-03 15:28:12 -0300

    ProfileMenu component

---
+++ src/components/ProfileMenu/test.tsx
@@ -0,0 +1,29 @@
+import { screen } from '@testing-library/react'
+import { renderWithTheme } from 'utils/tests/helpers'
+
+import theme from 'styles/theme'
+
+import ProfileMenu from '.'
+
+describe('<ProfileMenu />', () => {
+	it('Should be render the heading', () => {
+		renderWithTheme(<ProfileMenu />)
+
+		expect(
+			screen.getByRole('link', { name: /my profile/i })
+		).toBeInTheDocument()
+
+		expect(screen.getByRole('link', { name: /my cards/i })).toBeInTheDocument()
+		expect(screen.getByRole('link', { name: /my orders/i })).toBeInTheDocument()
+		expect(screen.getByRole('link', { name: /sign out/i })).toBeInTheDocument()
+	})
+
+	it('Should render the menu with an active link defined', () => {
+		renderWithTheme(<ProfileMenu activeLink="/profile/cards" />)
+
+		expect(screen.getByRole('link', { name: /my cards/i })).toHaveStyle({
+			background: theme.colors.primary,
+			color: theme.colors.white
+		})
+	})
+})
