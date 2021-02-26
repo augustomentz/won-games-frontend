6b32dc2f25cf17093a7c26c4a69c7afa2aba86a5
Augusto Mentz <augustohmn.tech@gmail.com>
2021-02-26 16:47:19 -0300

    CartDropdown component

---
+++ src/components/CartDropdown/styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+import { Wrapper as CartListWrapper } from 'components/CartList/styles'
+
+export const Wrapper = styled.main`
+	${CartListWrapper} {
+		width: 56rem;
+	}
+`
